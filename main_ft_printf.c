8b77fcf725581912dc85686ee5a33db2da32427b
tayou89 <ysh00718@gmail.com>
2023-01-21 17:49:29 +0900

    23/01/21 17:49

---
+++ main_ft_printf.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayou <tayou@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 20:27:20 by tayou             #+#    #+#             */
+/*   Updated: 2023/01/21 17:30:52 by tayou            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../ft_printf/ft_printf.h"
+#include <limits.h>
+/*
+int	main(void)
+{
+	int	return_printf;
+	int return_ft_printf;
+
+	return_printf = printf("%p\n%p\n", LONG_MIN, LONG_MAX);
+	return_ft_printf = ft_printf("%p\n%p\n", LONG_MIN, LONG_MAX);
+
+	return (0);
+}
+*/	
+int	main(void)
+{
+	char			*name;
+	void			*address;
+	int				number;
+	unsigned int	unsigned_number;
+	int				return_printf;
+	int				return_ft_printf;
+
+	name = "Tom";
+	address = name;
+	number = -2147483648;
+	unsigned_number = 0;
+	return_printf = printf("name: %s, address: %p, number: %d, unsigned_number: %u,\nhexa_number: %x, hexa_number: %X, percent_sign: %%\n", name, address, number, unsigned_number, unsigned_number, unsigned_number);
+	return_ft_printf = ft_printf("name: %s, address: %p, number: %d, unsigned_number: %u,\nhexa_number: %x, hexa_number: %X, percent_sign: %%\n", name, address, number, unsigned_number, unsigned_number, unsigned_number);
+
+	printf("return_printf: %d\n", return_printf);
+	printf("return_ft_printf: %d\n", return_ft_printf);
+
+	return (0);
+}
